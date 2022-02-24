aed77388fde6684743ca262de80628d9a2f40142
ANGEL R <>
2022-02-24 14:54:54 +0100

    VERSIÓN 1.1

--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -1,12 +1,12 @@
-package es.maestre;
+package cuentas.clientes;
 
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        Cuenta miCuenta;
         double saldoActual;
 
-        miCuenta = new CCuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        miCuenta = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
         
         System.out.println("El saldo actual es:"+ miCuenta.estado() );
 
