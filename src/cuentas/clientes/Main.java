af01d102d539c2ea8e38748eec4c2b50124dae97
ANGEL R <>
2022-02-24 17:07:14 +0100

    VERSIÓN 1.2

--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -3,23 +3,27 @@ package cuentas.clientes;
 public class Main {
 
     public static void main(String[] args) {
-        Cuenta miCuenta;
+        operativa_cuenta();
+    }
+
+    protected static void operativa_cuenta() {
+        Cuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
         
-        System.out.println("El saldo actual es:"+ miCuenta.estado() );
+        System.out.println("El saldo actual es:"+ cuenta1.estado() );
 
         try {
-            miCuenta.retirar(2300);
-            System.out.println("El saldo actual es:"+ miCuenta.estado() );
+            cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
-            System.out.println("El saldo actual es:"+ miCuenta.estado() );
+            cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
